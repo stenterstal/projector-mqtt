b71b6764f1d71762fc79488048b41603fb909129
Sten <mail@stenterstal.com>
2025-02-03 21:51:52 +0100

    Logging refactor

---
+++ log.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from enum import Enum
+
+
+# Colors
+grey = "\x1b[38;20m"
+yellow = "\x1b[33;20m"
+red = "\x1b[31;20m"
+bold_red = "\x1b[31;1m"
+reset = "\x1b[0m"
+
+class LogPrefix(str, Enum):
+    projector = 'PROJ'
+    dashboard = 'DASH'
+    mqtt = 'MQTT'
+
+class Logger:
+    def __init__(self, prefix: LogPrefix):
+        self.prefix = prefix
+
+    def info(self, message: str):
+        log_date = datetime.now().strftime("%H:%M:%S")
+        print(grey + "[" + log_date + "][" + self.prefix + "] " + message + reset)
+
+    def warn(self, message: str):
+        log_date = datetime.now().strftime("%H:%M:%S")
+        print(yellow + "[" + log_date + "]["+ self.prefix + "] " + message + reset)
+
+    def error(self, message: str):
+        log_date = datetime.now().strftime("%H:%M:%S")
+        print(red + "[" + log_date + "][" + self.prefix + "] " + message + reset)
--- projector/projector.py
+++ projector/projector.py
@@ -3,6 +3,7 @@ import json
 import paho.mqtt.client as mqtt
 
 from config import config_parser
+from log import Logger, LogPrefix
 from payloads.discovery import make_discovery_message
 from projector.lightcrafter.dpp2607 import *
 from projector.lightcrafter.constants import SlaveAddr, IODebug
@@ -13,6 +14,10 @@ MQTT_DEVICE_ID = "912f98b36b4f4776b785210671a78e5e"
 class Projector:
     def __init__(self):
         self.config = config_parser.read_config()
+
+        # Initiate logger
+        self.mqtt_log = Logger(LogPrefix.mqtt)
+        self.proj_log = Logger(LogPrefix.projector)
 
         self.mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
         self.mqttc.on_connect = self.on_connect
@@ -26,9 +31,9 @@ class Projector:
 
     def on_connect(self, client, userdata, flags, reason_code, properties):
         if reason_code != "Success":
-            print(f"[MQTT] Something went wrong: Connected with result code {reason_code}")
+            self.mqtt_log.error(f"Something went wrong: Connected with result code {reason_code}")
         else:
-            print("[MQTT] Successfully connected")
+            self.mqtt_log.info("Successfully connected")
 
         if self.config['mqtt_autodiscovery'] == 'true':
             self.homeassistant_discovery()
@@ -39,11 +44,11 @@ class Projector:
         payload = msg.payload.decode('utf-8')
         if msg.topic == "homeassistant/switch/%s/set" % MQTT_DEVICE_ID:
             if payload == "ON":
-                print("[MQTT] Received turn on")
-                projector_turn_on()
+                self.mqtt_log.info("Received turn on")
+                self.projector_turn_on()
             elif payload == "OFF":
-                print("[MQTT] Received turn off")
-                projector_turn_off()
+                self.mqtt_log.info("Received turn off")
+                self.projector_turn_off()
 
     def homeassistant_discovery(self):
         discovery_message = make_discovery_message(MQTT_DEVICE_ID)
@@ -52,28 +57,24 @@ class Projector:
         self.mqttc.publish("homeassistant/device/%s/config" % MQTT_DEVICE_ID, string, qos=1)
         print("[MQTT] Published config to home assistant autodiscovery")
 
-#
-#   (STATIC) Projector i2c functions
-#
+    def projector_turn_on(self):
+        DPP2607_Open()
+        DPP2607_SetSlaveAddr(SlaveAddr)
+        DPP2607_SetIODebug(IODebug)
+        DPP2607_Write_LedCurrentRed(255)
+        DPP2607_Write_LedCurrentGreen(255)
+        DPP2607_Write_LedCurrentBlue(255)
+        DPP2607_Write_PropagateLedCurrents(1)
+        DPP2607_Close()
+        self.proj_log.info("Set screen brightness to 255 (turn-on)")
 
-def projector_turn_on():
-    DPP2607_Open()
-    DPP2607_SetSlaveAddr(SlaveAddr)
-    DPP2607_SetIODebug(IODebug)
-    DPP2607_Write_LedCurrentRed(255)
-    DPP2607_Write_LedCurrentGreen(255)
-    DPP2607_Write_LedCurrentBlue(255)
-    DPP2607_Write_PropagateLedCurrents(1)
-    DPP2607_Close()
-    print('[PROJ] Set screen brightness to 255 (turn-on)')
-
-def projector_turn_off():
-    DPP2607_Open()
-    DPP2607_SetSlaveAddr(SlaveAddr)
-    DPP2607_SetIODebug(IODebug)
-    DPP2607_Write_LedCurrentRed(0)
-    DPP2607_Write_LedCurrentGreen(0)
-    DPP2607_Write_LedCurrentBlue(0)
-    DPP2607_Write_PropagateLedCurrents(1)
-    DPP2607_Close()
-    print('[PROJ] Set screen brightness to 0 (turn-off)')
+    def projector_turn_off(self):
+        DPP2607_Open()
+        DPP2607_SetSlaveAddr(SlaveAddr)
+        DPP2607_SetIODebug(IODebug)
+        DPP2607_Write_LedCurrentRed(0)
+        DPP2607_Write_LedCurrentGreen(0)
+        DPP2607_Write_LedCurrentBlue(0)
+        DPP2607_Write_PropagateLedCurrents(1)
+        DPP2607_Close()
+        self.proj_log.info("Set screen brightness to 0 (turn-off)")
