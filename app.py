59dff272cd08e4d61311eab81357607a243fd6bc
Sten <mail@stenterstal.com>
2025-02-03 21:35:02 +0100

    Refactoring

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 import threading
 
 from interface.flask import app
-from mqtt.mqtt import Mqtt
+from projector.projector import Projector
 
 
 def run_flask():
     app.run(debug=True, use_reloader=False, host="0.0.0.0", port=5000)
 
-def run_mqtt():
-    Mqtt()
+def run_projector_mqtt():
+    Projector()
 
 if __name__ == '__main__':
     # Create threads for Flask and normal app
     flask_thread = threading.Thread(target=run_flask)
-    normal_thread = threading.Thread(target=run_mqtt)
+    projector_thread = threading.Thread(target=run_projector_mqtt)
 
     # Start both threads
     flask_thread.start()
-    normal_thread.start()
+    projector_thread.start()
 
     # Wait for both threads to finish
     flask_thread.join()
-    normal_thread.join()
+    projector_thread.join()
--- projector/projector.py
+++ projector/projector.py
@@ -1,48 +1,80 @@
-import subprocess
-from enum import Enum
+import json
 
+import paho.mqtt.client as mqtt
+
+from config import config_parser
+from payloads.discovery import make_discovery_message
 from projector.lightcrafter.dpp2607 import *
 from projector.lightcrafter.constants import SlaveAddr, IODebug
 
+MQTT_DEVICE_ID = "912f98b36b4f4776b785210671a78e5e"
 
-class ProjectorState(str, Enum):
-    stopped = 'stopped'
-    running = 'running'
-
-    def __str__(self) -> str:
-        return self.value
 
 class Projector:
     def __init__(self):
-        self.state = ProjectorState('stopped')
+        self.config = config_parser.read_config()
+        self.projector = Projector()
 
-    def toggle(self):
-        if self.state == ProjectorState.stopped:
-            self.turn_on()
+        self.mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        self.mqttc.on_connect = self.on_connect
+        self.mqttc.on_message = self.on_message
+
+        self.mqttc.username_pw_set(self.config["mqtt_user"], self.config["mqtt_password"])
+
+        self.mqttc.connect(self.config["mqtt_address"], int(self.config["mqtt_port"]), 60)
+
+        self.mqttc.loop_forever()
+
+    def on_connect(self, client, userdata, flags, reason_code, properties):
+        if reason_code != "Success":
+            print(f"[MQTT] Something went wrong: Connected with result code {reason_code}")
         else:
-            self.turn_off()
+            print("[MQTT] Successfully connected")
 
-    def turn_on(self):
-        DPP2607_Open()
-        DPP2607_SetSlaveAddr(SlaveAddr)
-        DPP2607_SetIODebug(IODebug)
-        DPP2607_Write_LedCurrentRed(255)
-        DPP2607_Write_LedCurrentGreen(255)
-        DPP2607_Write_LedCurrentBlue(255)
-        DPP2607_Write_PropagateLedCurrents(1)
-        DPP2607_Close()
-        print('[PROJ] Set screen brightness to 255 (turn-on)')
+        if self.config['mqtt_autodiscovery'] == 'true':
+            self.homeassistant_discovery()
 
-    def turn_off(self):
-        DPP2607_Open()
-        DPP2607_SetSlaveAddr(SlaveAddr)
-        DPP2607_SetIODebug(IODebug)
-        DPP2607_Write_LedCurrentRed(0)
-        DPP2607_Write_LedCurrentGreen(0)
-        DPP2607_Write_LedCurrentBlue(0)
-        DPP2607_Write_PropagateLedCurrents(1)
-        DPP2607_Close()
-        print('[PROJ] Set screen brightness to 0 (turn-off)')
+        client.subscribe("homeassistant/switch/%s/#" % MQTT_DEVICE_ID)
 
-    def __getstate__(self) -> ProjectorState:
-        return self.state
+    def on_message(self, client, userdata, msg):
+        payload = msg.payload.decode('utf-8')
+        if msg.topic == "homeassistant/switch/%s/set" % MQTT_DEVICE_ID:
+            if payload == "ON":
+                print("[MQTT] Received turn on")
+                projector_turn_on()
+            elif payload == "OFF":
+                print("[MQTT] Received turn off")
+                projector_turn_off()
+
+    def homeassistant_discovery(self):
+        discovery_message = make_discovery_message(MQTT_DEVICE_ID)
+        string = json.dumps(discovery_message)
+
+        self.mqttc.publish("homeassistant/device/%s/config" % MQTT_DEVICE_ID, string, qos=1)
+        print("[MQTT] Published config to home assistant autodiscovery")
+
+#
+#   (STATIC) Projector i2c functions
+#
+
+def projector_turn_on():
+    DPP2607_Open()
+    DPP2607_SetSlaveAddr(SlaveAddr)
+    DPP2607_SetIODebug(IODebug)
+    DPP2607_Write_LedCurrentRed(255)
+    DPP2607_Write_LedCurrentGreen(255)
+    DPP2607_Write_LedCurrentBlue(255)
+    DPP2607_Write_PropagateLedCurrents(1)
+    DPP2607_Close()
+    print('[PROJ] Set screen brightness to 255 (turn-on)')
+
+def projector_turn_off():
+    DPP2607_Open()
+    DPP2607_SetSlaveAddr(SlaveAddr)
+    DPP2607_SetIODebug(IODebug)
+    DPP2607_Write_LedCurrentRed(0)
+    DPP2607_Write_LedCurrentGreen(0)
+    DPP2607_Write_LedCurrentBlue(0)
+    DPP2607_Write_PropagateLedCurrents(1)
+    DPP2607_Close()
+    print('[PROJ] Set screen brightness to 0 (turn-off)')
