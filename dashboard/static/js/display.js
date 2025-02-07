7c766b466241ba0102f8be83d841544e917c5d9e
Sten <mail@stenterstal.com>
2025-02-07 20:02:46 +0100

    Notification container

---
+++ dashboard/static/js/display.js
@@ -0,0 +1,49 @@
+function clearNotifications(){
+    // Clear all previous disruptions
+    $('#notifications').empty()
+}
+
+function addNotification(title, subtitle, icontype=null){
+    // Construct element
+    let containerElement = $('<div class="notification">')
+    // Text
+    let textElement = $('<div class="text">')
+    textElement.append($('<p>').text(title))
+    textElement.append($('<span>').text(subtitle))
+    // Icon
+    let iconElement = getIcon(icontype)
+
+    // Append to container
+    containerElement.append(iconElement)
+    containerElement.append(textElement)
+
+    // Append to container
+    $('#notifications').append(containerElement)
+}
+
+function getIcon(icontype=null){
+    let containerElement = $('<div class="icon-container">')
+    switch (icontype){
+        case 'disruption':
+            // Triangle exclamation (https://icons.getbootstrap.com/icons/exclamation-triangle-fill/)
+            containerElement.append(
+                $('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">\n' +
+                  '<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>\n' +
+                '</svg>'))
+            // Train (https://icons.getbootstrap.com/icons/train-front/)
+            containerElement.append(
+                $('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-train-front" viewBox="0 0 16 16">\n' +
+                    '<path d="M5.621 1.485c1.815-.454 2.943-.454 4.758 0 .784.196 1.743.673 2.527 1.119.688.39 1.094 1.148 1.094 1.979V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V4.583c0-.831.406-1.588 1.094-1.98.784-.445 1.744-.922 2.527-1.118m5-.97C8.647.02 7.353.02 5.38.515c-.924.23-1.982.766-2.78 1.22C1.566 2.322 1 3.432 1 4.582V13.5A2.5 2.5 0 0 0 3.5 16h9a2.5 2.5 0 0 0 2.5-2.5V4.583c0-1.15-.565-2.26-1.6-2.849-.797-.453-1.855-.988-2.779-1.22ZM5 13a1 1 0 1 1-2 0 1 1 0 0 1 2 0m0 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0m7 1a1 1 0 1 0-1-1 1 1 0 1 0-2 0 1 1 0 0 0 2 0 1 1 0 0 0 1 1M4.5 5a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 .5.5h3V5zm4 0v3h3a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5zM3 5.5A1.5 1.5 0 0 1 4.5 4h7A1.5 1.5 0 0 1 13 5.5v2A1.5 1.5 0 0 1 11.5 9h-7A1.5 1.5 0 0 1 3 7.5zM6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1z"/>\n' +
+                '</svg>'))
+            break
+        default:
+            // Info icon (https://icons.getbootstrap.com/icons/info-circle/)
+            containerElement.append(
+                $('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">\n' +
+                  '<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>\n' +
+                  '<path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>\n' +
+                '</svg>'))
+            break
+    }
+    return containerElement
+}
